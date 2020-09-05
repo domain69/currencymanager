95e1508415d8758d0e8158e251a98e49979c4e16
Hitesh shetty <hitesh.shetty2011@gmail.com>
2020-09-05 10:49:56 +0530

    Updates

---
+++ precache-manifest.e81064505041aded9ead8f3018e4db24.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45663ea81e814fe661cbf7abbc035a77",
+    "url": "/currencymanager/index.html"
+  },
+  {
+    "revision": "c083a3086cbb2542255a",
+    "url": "/currencymanager/static/css/2.d4e7eff5.chunk.css"
+  },
+  {
+    "revision": "46882abdecbb32ea4ce0",
+    "url": "/currencymanager/static/css/main.5ef75bbb.chunk.css"
+  },
+  {
+    "revision": "c083a3086cbb2542255a",
+    "url": "/currencymanager/static/js/2.a8d18e8c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/currencymanager/static/js/2.a8d18e8c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46882abdecbb32ea4ce0",
+    "url": "/currencymanager/static/js/main.a834f967.chunk.js"
+  },
+  {
+    "revision": "3092ec5c00d691db9129",
+    "url": "/currencymanager/static/js/runtime-main.89aabe20.js"
+  }
+]);
